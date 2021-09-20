1d72c021c4050b7cf39058666c77b5cfbc8a576d
neves-gabriel <gabrielrn.mobres@gmail.com>
2021-09-20 06:57:00 -0300

    add: TopBar dropdown menu

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { IoChevronDown } from "react-icons/io5";
 
 export default function TopBar() {
 
   const [avatar,setAvatar] = useState('');
+  const [isActive, setIsActive] = useState(false);
+  const onClick = () => setIsActive(!isActive);
 
   useEffect(() => {
     setAvatar( JSON.parse(localStorage.getItem('user')).avatar );
@@ -17,10 +19,27 @@ export default function TopBar() {
       <Logo>
         <Link to="/timeline" style={{ textDecoration: 'none' }}><h2>Linkr</h2></Link>
       </Logo>
-        <UserBox>
-          <IoChevronDown size="1.8em" /> 
-          <UserAvatar src={avatar} />
-        </UserBox>
+      <div className="container">
+        <MenuContainer>
+          <UserBox onClick={onClick} className="menu-trigger">
+            <IoChevronDown size="1.8em" /> 
+            <UserAvatar src={avatar} />
+          </UserBox>
+        <Menu active={isActive}>
+          <ul>
+            <li>
+              <Link to="/my-posts">My posts</Link>
+            </li>
+            <li>
+              <Link to="/my-likes">My likes</Link>
+            </li>
+            <li>
+              <Link to="/">Logout</Link>
+            </li>
+          </ul>
+        </Menu>
+      </MenuContainer>
+    </div>
     </Top>
 
   );
@@ -56,17 +75,59 @@ const Logo = styled.div`
   }
 `;
 
-const UserBox = styled.div`
+const UserBox = styled.button`
   display: flex;
   justify-content: space-between;
   align-items: center;
   color: white;
   width: 90px;
   height: auto;
+  border: none;
+  transition: box-shadow 0.4s ease;
 `;
 
 const UserAvatar = styled.img`
   width: 50px;
   height: 50px;
   border-radius: 26.5px;
+`;
+
+const MenuContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Menu = styled.nav`
+  position: absolute;
+  top: 70px;
+  right: -17px;
+  width: 150px;
+  height: auto;
+  background: #171717;
+  border-radius: 0px 0px 20px 20px;
+  opacity: 0;
+  visibility: hidden;
+  transform: translateY(-20px);
+  transition: opacity 0.4s ease, transform 0.4s ease, visibility 0.4s;
+  :active {
+    opacity: 1;
+    visibility: visible;
+    transform: translateY(0);
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  li {
+    border-bottom: 1px solid #dddddd;
+  }
+  Link {
+    text-decoration: none;
+    color: #333333;
+    padding: 15px 20px;
+    display: block;
+  }
 `;
