35c159bc2fab6baf2f0b0508a2002bbfa34f654a
neves-gabriel <gabrielrn.mobres@gmail.com>
2021-09-20 11:19:17 -0300

    add: logout function

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,45 +1,55 @@
 import styled from "styled-components";
 import React, { useState, useEffect, useRef } from "react";
 import { Link, useHistory } from "react-router-dom";
-import { IoChevronDown } from "react-icons/io5";
+import { IoChevronDown, IoChevronUp } from "react-icons/io5";
 
 export default function TopBar() {
 
   const [avatar,setAvatar] = useState('');
   const [isActive, setIsActive] = useState(false);
+
+  const history = useHistory();
+  
   const onClick = () => setIsActive(!isActive);
 
   useEffect(() => {
+
+    if (!localStorage.getItem("auth")) {
+      alert("Faça login antes!");
+      history.push("/");
+      return;
+    }
+
     setAvatar( JSON.parse(localStorage.getItem('user')).avatar );
   }, []);
+
+  function Logout() {
+    localStorage.clear();
+  }
 
   return ( 
 
     <Top>
       <Logo>
-        <Link to="/timeline" style={{ textDecoration: 'none' }}><h2>Linkr</h2></Link>
+        <StyledLink to="/timeline"><h2>Linkr</h2></StyledLink>
       </Logo>
-      <div className="container">
-        <MenuContainer>
-          <UserBox onClick={onClick} className="menu-trigger">
-            <IoChevronDown size="1.8em" /> 
+          <UserBox onClick={onClick} >
+            { isActive ? <IoChevronUp size="1.8em"/> : <IoChevronDown size="1.8em"/> }
             <UserAvatar src={avatar} />
           </UserBox>
         <Menu active={isActive}>
           <ul>
             <li>
-              <Link to="/my-posts">My posts</Link>
+              <StyledLink to="/my-posts">My posts</StyledLink>
             </li>
             <li>
-              <Link to="/my-likes">My likes</Link>
+              <StyledLink to="/my-likes">My likes</StyledLink>
             </li>
             <li>
-              <Link to="/">Logout</Link>
+              <StyledLink to="/" onClick={Logout} >Logout</StyledLink>
             </li>
           </ul>
         </Menu>
-      </MenuContainer>
-    </div>
     </Top>
 
   );
@@ -64,7 +74,7 @@ const Logo = styled.div`
   align-items: center;
   justify-content: center;
   font-weight: bold;
-  color: white;
+  color: #FFFFFF;
   background-color: #151515;
   letter-spacing: 4px;
   h2 {
@@ -84,6 +94,8 @@ const UserBox = styled.button`
   height: auto;
   border: none;
   transition: box-shadow 0.4s ease;
+  background-color: #151515;
+  cursor: pointer;
 `;
 
 const UserAvatar = styled.img`
@@ -92,42 +104,40 @@ const UserAvatar = styled.img`
   border-radius: 26.5px;
 `;
 
-const MenuContainer = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
 const Menu = styled.nav`
+  padding: 10px 0 17px 28px;
   position: absolute;
-  top: 70px;
+  top: 80px;
   right: -17px;
   width: 150px;
   height: auto;
   background: #171717;
   border-radius: 0px 0px 20px 20px;
-  opacity: 0;
-  visibility: hidden;
-  transform: translateY(-20px);
+  opacity: ${props => (props.active ? '1' : '0')};
+  visibility: ${props => (props.active ? 'visible' : 'hidden')};
+  transform: ${props => (props.active ? 'translateY(-20px)' : 'translateY(0);')};
   transition: opacity 0.4s ease, transform 0.4s ease, visibility 0.4s;
-  :active {
-    opacity: 1;
-    visibility: visible;
-    transform: translateY(0);
-  }
+  font-family: Lato;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 17px;
+  line-height: 20px;
+  letter-spacing: 0.05em;
+  color: #FFFFFF;
   ul {
     list-style: none;
     padding: 0;
     margin: 0;
   }
   li {
-    border-bottom: 1px solid #dddddd;
+    margin: 5px 0 5px 0;
   }
-  Link {
+`;
+
+const StyledLink = styled(Link)`
     text-decoration: none;
-    color: #333333;
-    padding: 15px 20px;
-    display: block;
-  }
+    color: #FFFFFF;
+    &:focus, &:hover, &:visited, &:link, &:active {
+        text-decoration: none;
+    }
 `;
